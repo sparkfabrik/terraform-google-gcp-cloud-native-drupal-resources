d985c5c4fa4b82472b387c36acbaf6245cbe639c
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2024-11-28 15:33:46 +0100

    Fix database secret name mapping for Drupal projects in outputs

--- outputs.tf
+++ outputs.tf
@@ -33,7 +33,7 @@ locals {
   database_secrets_map = {
     for p in var.drupal_projects_list : "${p.project_name}-${p.gitlab_project_id}-${p.release_branch_name}" => {
       secret_name = try(
-        kubernetes_secret.database_secret_name["${p.project_name}-${p.gitlab_project_id}-${p.release_branch_name}-db-user"].metadata[0].name,
+        kubernetes_secret.database_secret_name["drupal-${p.release_branch_name}-${p.gitlab_project_id}-db-user"].metadata[0].name,
         null
       )
     }
