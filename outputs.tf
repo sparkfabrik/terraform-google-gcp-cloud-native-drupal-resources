cf20259312161ca8d692f886b7fa1872b26073e5
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2024-11-28 11:55:11 +0100

    db credentials

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ locals {
       # Example:
       namespace            = p.kubernetes_namespace == null ? "${p.project_name}-${p.gitlab_project_id}-${p.release_branch_name}" : p.kubernetes_namespace
       bucket_credentials   = module.drupal_buckets[0].buckets_access_credentials["${p.project_name}-${p.gitlab_project_id}-${p.release_branch_name}-drupal"]
-      database_credentials = [for cred in module.drupal_databases_and_users[*].sql_users_creds : cred if cred.database == "${p.project_name}_${p.gitlab_project_id}_${p.release_branch_name}_dp"][0]
+      database_credentials = [for cred in toset(module.drupal_databases_and_users[*].sql_users_creds) : cred if cred.database == "${p.project_name}_${p.gitlab_project_id}_${p.release_branch_name}_dp"][0]
     }
   }
 }
