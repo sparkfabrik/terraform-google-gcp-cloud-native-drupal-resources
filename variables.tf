cd983d3572dc138d80903952694de726589c7005
Fabrizio Cafolla <developer@fabriziocafolla.com>
2025-02-18 15:02:21 +0100

    refs #3439: Add default ACME allow network policy (#52)

--- variables.tf
+++ variables.tf
@@ -75,6 +75,12 @@ variable "drupal_projects_list" {
     bucket_obj_vwr                       = optional(list(string), [])
     bucket_soft_delete_retention_seconds = optional(number, 0)
     network_policy                       = optional(string, "")
+    network_policy_acme_port             = optional(number, 8089)
+    network_policy_acme_labels = optional(map(string),
+      {
+        "acme.cert-manager.io/http01-solver" = "true"
+      }
+    )
   }))
 
   validation {
